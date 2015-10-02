54c4d77f0e557e150cdad3f4a88466835b032bcf
unknown <BSB007@CZC339322L.aytomadrid.munimadrid.es>
2015-10-02 13:37:03 +0200

    MOD: json response for devise sign_up

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
+  before_filter :authenticate_user!, :except => [:create], :if => Proc.new { |c| c.request.format == 'application/json' }
+  respond_to :json
+
+  prepend_view_path 'app/views/devise'
+
+  def create
+    build_resource(sign_up_params)
+    if resource.save
+      sign_in resource
+      render :status => 200,
+             :json => { :success => true,
+                        :info => "Registered",
+                        :email => resource.email}
+    else
+      render :status => :unprocessable_entity,
+             :json => { :success => false,
+                        :info => resource.errors }
+    end
+  end
 
 end
